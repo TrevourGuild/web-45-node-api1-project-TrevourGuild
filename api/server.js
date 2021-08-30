b65d0e56657c532b09cba74241b5e69050dbfe9c
TrevourGuild <trevorr.guildd@gmail.com>
2021-08-30 13:59:26 -0700

    completed first api call

--- api/server.js
+++ api/server.js
@@ -1,3 +1,46 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const Users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) =>{
+    res.status(200).json({message: 'hey there trevour' })
+})
+
+server.post('/api/users' ,(req, res) =>{
+    res.json('finds users')
+})
+
+server.get('/api/users', (req,res)=>{
+    Users.find()
+    .then(users =>{
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: err.message })
+      })
+})
+
+server.get('/api/users/:id', (req,res)=>{
+    res.json('retuns object of users with id')
+})
+
+server.delete('/api/users/:id', (req, res) =>{
+    res.json('deletes users')
+})
+
+server.put('/api/users/:id', (req, res) =>{
+    res.json('updates user')
+})
+
+
+
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
