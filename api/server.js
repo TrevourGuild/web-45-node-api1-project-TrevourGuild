0c8c30d8397c62839ab0bed7af7e7805fa5a84d1
TrevourGuild <trevorr.guildd@gmail.com>
2021-08-30 16:08:27 -0700

    finished project

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-const Users = require('./users/model')
+const Users = require('./users/model.js')
 
 const server = express()
 
@@ -10,8 +10,20 @@ server.get('/', (req, res) =>{
     res.status(200).json({message: 'hey there trevour' })
 })
 
-server.post('/api/users' ,(req, res) =>{
-    res.json('finds users')
+server.post('/api/users', (req, res) =>{
+    const newUser = req.body
+    Users.insert(newUser)
+    .then(user =>{
+        if (user){
+            res.status(201).json(user)
+        }else{
+            res.status(400).json({message: "Please provide name and bio for the user"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: err.message })
+    })
 })
 
 server.get('/api/users', (req,res)=>{
@@ -26,11 +38,35 @@ server.get('/api/users', (req,res)=>{
 })
 
 server.get('/api/users/:id', (req,res)=>{
-    res.json('retuns object of users with id')
+    Users.findById(req.params.id)
+    .then(user =>{
+        if(user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: err.message })
+      })
 })
 
 server.delete('/api/users/:id', (req, res) =>{
-    res.json('deletes users')
+    Users.remove(req.params.id)
+    .then(user =>{
+        if(user) {
+            res.status(200).json(user)
+        } else{
+            res.status(404).json({
+                message: `dog ${req.params.id} not real!!!`
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: err.message })
+      })
 })
 
 server.put('/api/users/:id', (req, res) =>{
